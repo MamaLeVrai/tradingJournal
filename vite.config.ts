4cab5a4fd644dc311ea1582c9659c9041e0a28a2
marius margueray <margueray.marius@gmail.com>
2025-10-05 20:17:39 +0200

    V1.07

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// minimal declaration to avoid needing full @types/node just for process here
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const process: any;
-
 export default defineConfig({
-  // Project site deployment at https://mamalevrai.github.io/tradingJournal/
   base: '/tradingJournal/',
   plugins: [react()],
 });
